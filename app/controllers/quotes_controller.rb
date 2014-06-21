169ce6b358e033c261dbd1203371b407aca3cb11
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-21 14:53:01 -0700

    creating a new quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,10 @@
 class QuotesController < Rulers::Controller
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
   def a_quote
     render :a_quote, :noun => :winking
   end
@@ -7,4 +13,15 @@ class QuotesController < Rulers::Controller
     quote_1 = Rulers::Model::FileModel.find(1)
     render :quote, :obj => quote_1
   end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Marco"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
 end
